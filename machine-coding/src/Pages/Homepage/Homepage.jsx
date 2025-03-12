f6bea7b33013d3d11452b213359de9025738cc93
Aayush Jangra <jangra.aayush@gmail.com>
2025-03-12 17:33:29 +0530

    Search option

--- machine-coding/src/Pages/Homepage/Homepage.jsx
+++ machine-coding/src/Pages/Homepage/Homepage.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import { ContentBox } from "../../components/ContentBox";
 import { questions } from "../../constants/questions";
 import "./homepageStyles.css";
+import { useDebounce } from "../../utils/debounce";
 
 const Homepage = () => {
+  const [search, setSearch] = useState("");
+  const [filteredQuestions, setFilteredQuestions] = useState(questions);
+
+  const debouncedSearch = useDebounce((value) => {
+    setFilteredQuestions(
+      questions.filter((ques) =>
+        ques.title.toLowerCase().includes(value.toLowerCase())
+      )
+    );
+  }, 300);
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    debouncedSearch(e.target.value);
+  };
+
   return (
-    <>
+    <div className="homepage-container">
       <h1>
         Welcome and explore the frequently asked machine coding round questions
       </h1>
+      <input
+        className="homepage-search-input"
+        type="text"
+        value={search}
+        onChange={handleSearchChange}
+        placeholder="Search 🔎"
+      />
       <div className="homepage-questions">
-        {questions.map((ques) => (
+        {filteredQuestions.map((ques) => (
           <ContentBox
             key={ques.path}
             title={ques.title}
@@ -20,7 +45,7 @@ const Homepage = () => {
           />
         ))}
       </div>
-    </>
+    </div>
   );
 };
 export default Homepage;
